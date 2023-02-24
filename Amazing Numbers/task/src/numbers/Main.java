66f79494b21f82d130cd838cfc39ad8161a27a2a
SunnyPanda <kashtanka.nya@gmail.com>
2023-02-24 22:38:10 +0200

    Stage 2/8: Duck numbers

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -12,19 +12,25 @@ public class Main {
             System.out.println("This number is not natural!");
             return;
         }
+        System.out.printf("""
+                Properties of %d
+                        even: %b
+                         odd: %b
+                        buzz: %b
+                        duck: %b""", number, isEven(number), !isEven(number), isBuzz(number), isDuck(number));
+    }
 
-        String oddOrEven = number % 2 == 0 ? "Even" : "Odd";
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    private static boolean isBuzz(int number) {
         boolean divBy7 = number % 7 == 0;
         boolean endsWith7 = number % 10 == 7;
-        boolean isBuzz = divBy7 || endsWith7;
-
-        System.out.printf("This number is %s.\n", oddOrEven);
-        System.out.printf("It is%sa Buzz number.\n", isBuzz ? " " : " not ");
-
-        System.out.println("Explanation:");
-        if (divBy7 && endsWith7) System.out.printf("%d is divisible by 7 and ends with 7.\n", number);
-        else if (divBy7) System.out.printf("%d is divisible by 7.\n", number);
-        else if (endsWith7) System.out.printf("%d is ends with 7.\n", number);
-        else System.out.printf("%d is neither divisible by 7 nor does it end with 7.\n", number);
+        return divBy7 || endsWith7;
     }
-}
+
+    private static boolean isDuck(int number) {
+        return String.valueOf(number).lastIndexOf("0") > 0;
+    }
+ }
